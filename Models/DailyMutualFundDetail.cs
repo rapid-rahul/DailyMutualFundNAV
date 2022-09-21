b42938c83ee695d78ddc26157ec4fbbea4bf4a5b
Rahul Soni <41254014+rapid-rahul@users.noreply.github.com>
2022-09-21 16:38:09 +0530

    Add files via upload

---
+++ Models/DailyMutualFundDetail.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailyMutualFundNAV.Models
+{
+    public class DailyMutualFundDetail
+    {
+
+        [Key]
+        public int MutualFundId { get; set; }
+        public string MutualFundName { get; set; }
+        public double MutualFundvalue { get; set; }
+    }
+}
---
+++ Models/MutualFundContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailyMutualFundNAV.Models
+{
+    public class MutualFundContext:DbContext
+    {
+        
+        public MutualFundContext() { }
+            public MutualFundContext(DbContextOptions options) : base(options) { }
+
+            public DbSet<DailyMutualFundDetail> DailyMutualFundDetails { get; set; }
+    }
+    
+}
