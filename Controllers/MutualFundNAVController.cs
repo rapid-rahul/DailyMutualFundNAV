ba35bea1a9f1b5a9ede7d05878951876f1d23e04
Rahul Soni <41254014+rapid-rahul@users.noreply.github.com>
2022-09-21 16:34:30 +0530

    Add files via upload

---
+++ Controllers/MutualFundNAVController.cs
@@ -0,0 +1,64 @@
+﻿using DailyMutualFundNAV.Models;
+using DailyMutualFundNAV.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailyMutualFundNAV.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MutualFundNAVController : ControllerBase
+    {
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(MutualFundNAVController));
+        private readonly IMutualFundRepository mutualFundRepository;
+       
+        public MutualFundNAVController(IMutualFundRepository _mutualFundRepository)
+        {
+            _log4net.Info("MutualFundNAVController Initiated");
+            mutualFundRepository = _mutualFundRepository;
+        }
+        [HttpGet]
+        [Route("mutualfundname")]
+        public IActionResult GetMutualFundByName(string mutualfundname)
+        {
+            try
+            {
+                if (mutualfundname == null)
+                {
+                    _log4net.Info("MutualFund Null Name.");
+                    return BadRequest("Name Cannot be null");
+                }
+                _log4net.Info("In MutualFundNAV Controller, " + mutualfundname + " is searched");
+                var mutualFundData = mutualFundRepository.GetMutualFundByNameRepository(mutualfundname);
+                if (mutualFundData == null)
+                {
+                    _log4net.Info(mutualfundname + " is invalid MutualFund.");
+                    return NotFound("Invalid MutualFund Name");
+                    //return BadRequest();
+                }
+                else
+                {
+                    _log4net.Info(mutualfundname + " MutualFund Found.");
+                    return Ok(mutualFundData);
+                }
+                //return Ok(mutualFundData);
+            }
+            catch (Exception exception)
+            {
+                _log4net.Error("Exception Found=" + exception.Message);
+                return new StatusCodeResult(500);
+            }
+
+        }
+        [HttpGet]
+        public ActionResult GetAllMutualFund()
+        {
+            var l = mutualFundRepository.GetAllmutualFund();
+            return Ok(l);
+        }
+    }
+}
---
+++ Migrations/20220909144036_mig1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DailyMutualFundNAV.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DailyMutualFundDetails",
+                columns: table => new
+                {
+                    MutualFundId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MutualFundName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MutualFundvalue = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyMutualFundDetails", x => x.MutualFundId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailyMutualFundDetails");
+        }
+    }
+}
